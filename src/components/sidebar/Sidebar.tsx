c8fb6e457144076314262fc5259b7dbe4ec92688
bruhlord-s <treize.konstantin@yandex.ru>
2023-01-21 13:31:59 +0600

    init commit

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+import styles from "./sidebar.module.css";
+
+const Sidebar: FC = () => {
+  return <aside></aside>;
+};
+
+export default Sidebar;
---
+++ src/layouts/dashboard-layout/DashboardLayout.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+interface DashboardLayoutProps {
+  children: JSX.Element;
+}
+
+const DashboardLayout: FC<DashboardLayoutProps> = ({
+  children,
+}: DashboardLayoutProps) => {
+  return <div></div>;
+};
+
+export default DashboardLayout;
