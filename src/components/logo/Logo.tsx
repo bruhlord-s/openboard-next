a6f08a1cb1dcaa943a01de901eaea941b248b4dc
bruhlord-s <treize.konstantin@yandex.ru>
2023-01-21 13:42:50 +0600

    added logo component

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+import styles from "./logo.module.css";
+
+const Logo: FC = () => {
+  return <h1 className={styles.logo}>Openboard</h1>;
+};
+
+export default Logo;
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,9 +1,16 @@
 import React, { FC } from "react";
+import Logo from "../logo/Logo";
 
 import styles from "./sidebar.module.css";
 
 const Sidebar: FC = () => {
-  return <aside>a</aside>;
+  return (
+    <aside className={styles.sidebar}>
+      <div className={styles.sidebar__logo}>
+        <Logo />
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
