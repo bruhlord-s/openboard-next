9b1cb0f48ebb4ad5f05fa77d4469fca9dde208d2
bruhlord-s <treize.konstantin@yandex.ru>
2023-05-01 10:47:52 +0600

    added delete option for task attachments

--- src/components/task-attachments/TaskAttachments.tsx
+++ src/components/task-attachments/TaskAttachments.tsx
@@ -1,13 +1,77 @@
-import { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 
 import styles from "./taskAttachments.module.css";
+import Image from "next/image";
 
 const TaskAttachments: FC = () => {
+  const [attachments, setAttachments] = useState<any[]>([]);
+
+  const fileInputRef = React.useRef<HTMLInputElement>(null);
+
+  const handleInput = (e: React.FormEvent<HTMLInputElement>) => {
+    if (e.currentTarget.files!.length > 0) {
+      setAttachments([...attachments, ...Array.from(e.currentTarget.files!)]);
+    }
+  };
+
+  const removeAttachment = (index: number) => {
+    let attachmentsCopy = Array.from(attachments);
+    attachmentsCopy.splice(index, 1);
+    setAttachments(attachmentsCopy);
+  };
+
   return (
-    <div className={styles.form__attachments}>
-      <div className={styles.form__attachmentsHeader}>
-        <p className={styles.form__attachmentsLabel}>Attachments</p>
-        <p className={styles.form__attachmentsBtn}>Upload</p>
+    <div className={styles.attachments}>
+      <div className={styles.attachments__header}>
+        <p className={styles.attachments__label}>Attachments</p>
+        <p
+          className={styles.attachments__btn}
+          onClick={() => {
+            fileInputRef.current?.click();
+          }}
+        >
+          Upload
+        </p>
+        <input
+          type="file"
+          ref={fileInputRef}
+          hidden
+          onInput={(e) => handleInput(e)}
+          accept="image/png, image/jpeg"
+          multiple
+        />
+      </div>
+      <div className={styles.attachments__inner}>
+        {attachments?.length < 1 && (
+          <div className={styles.attachments__empty}>
+            <p>＞﹏＜</p>
+            <p>No attachments</p>
+          </div>
+        )}
+        {attachments?.length > 0 && (
+          <div className={styles.attachments__images}>
+            {attachments.map((attachment, i) => (
+              <div className={styles.attachments__attachment} key={i}>
+                <img
+                  className={styles.attachments__img}
+                  src={URL.createObjectURL(attachment)}
+                  alt="attachment"
+                />
+                <span
+                  className={styles.attachments__remove}
+                  onClick={() => removeAttachment(i)}
+                >
+                  <Image
+                    src={"icons/trash.svg"}
+                    alt="remove"
+                    width={20}
+                    height={20}
+                  />
+                </span>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
